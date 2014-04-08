7e22ecbcce6e226b54e8841c0ef077b6c808cce3
Simon Wood <simon.wood@holidayextras.com>
2014-04-08 12:16:35 +0100

    Added YeomanDomain.js and some basic code. Changed main.js to call domain when menu item is selected.

--- main.js
+++ main.js
@@ -8,22 +8,32 @@ define(function (require, exports, module) {
     var CMD_GENERATE_WITH_YEOMAN = "com.adobe.brackets.commands.yeoman.generateFromYeoman";
 
     var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus = brackets.getModule("command/Menus");
+        KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
+        Menus = brackets.getModule("command/Menus"),
+        NodeDomain = brackets.getModule("utils/NodeDomain"),
+        ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+    
+    var yeomanDomain = new NodeDomain("yeoman", ExtensionUtils.getModulePath(module, "node/YeomanDomain"));
 
     // Function to run when the menu item is clicked
     function _openYeoman() {
-        window.alert("Yeoman!");
+        yeomanDomain.exec("getVersion").done(function (version) {
+            window.alert("Yeoman!  Version: " + version);
+        }).fail(function (err) {
+            console.error("[brackets-yeoman-node] failed to run yeoman.getVersion", err);
+        });
+        
     }
 
     // First, register a command - a UI-less object associating an id to a handler
-    CommandManager.register("Generate with Yeoman...", CMD_GENERATE_WITH_YEOMAN, _openYeoman);
-
+    var generateYeomanCmd = CommandManager.register("Generate with Yeoman...", CMD_GENERATE_WITH_YEOMAN, _openYeoman),
+        fileMenu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+    
     // Then create a menu item bound to the command
     // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(CMD_GENERATE_WITH_YEOMAN);
-
-    // We could also add a key binding at the same time:
-    menu.addMenuItem(CMD_GENERATE_WITH_YEOMAN, "Ctrl-Alt-Y");
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
+    fileMenu.addMenuDivider();
+    fileMenu.addMenuItem(generateYeomanCmd);
+    
+    // Add key binding  (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
+    KeyBindingManager.addBinding(CMD_GENERATE_WITH_YEOMAN, {key: "Ctrl-Shift-Y"});
 });
